f3e4e652b4588cb58e7148b470f439ce0222aff6
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-04-04 10:40:13 +0100

    basic

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+""" Basic asynchronous coroutine"""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    wait = random.uniform(0, max_delay)
+    await asyncio.sleep(wait)
+    return wait
