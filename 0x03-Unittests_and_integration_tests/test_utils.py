84bebab49df07f4921bbed4ca23d34a471e44e80
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-04-25 00:12:34 +0100

    raise

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,13 @@ class TestAccessNestedMap(unittest.TestCase):
         test access nested map
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        [{}, ("a")],
+        [{"a": 1}, ("a", "b")]
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        test access nested map exception
+        """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
